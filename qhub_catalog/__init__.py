7e777d22b2acca264049ed0179275e60c79ee87a
eskild <42120229+iameskild@users.noreply.github.com>
2022-05-11 09:19:50 -0700

    Update basic tutorial, add datasets (#1)
    * Add datasets
    
    * Add dask_cluster
    
    * Update catalog
    
    * put scm version in ignore
    
    * add gcp dep
    
    Co-authored-by: tonyfast <tony.fast@gmail.com>

--- qhub_catalog/__init__.py
+++ qhub_catalog/__init__.py
@@ -3,7 +3,6 @@ from intake import open_catalog
 
 __all__ = ("catalog",)
 
-
-THIS = Path(__file__).parent
-CATALOG = THIS / "qhub_catalog.yml"
+THIS = Path(__file__).parent.resolve()
+CATALOG = THIS.joinpath("qhub_catalog.yml")
 catalog = open_catalog(CATALOG)
